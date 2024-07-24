2e9a95345c3698608f6a3be8a3480535a1dedc28
Maxiflexy <onyemax247@gmail.com>
2024-07-24 09:38:39 +0100

    updated findInstructor

--- src/main/java/com/maxiflexy/advancedhibernate/AdvancedHibernateApplication.java
+++ src/main/java/com/maxiflexy/advancedhibernate/AdvancedHibernateApplication.java
@@ -19,7 +19,8 @@ public class AdvancedHibernateApplication {
 	public CommandLineRunner commandLineRunner(AppDAO appDAO){
 
 		return runner -> {
-			createInstructor(appDAO);
+			//createInstructor(appDAO);
+			findInstructor(appDAO);
 		};
 	}
 
@@ -57,4 +58,13 @@ public class AdvancedHibernateApplication {
 		System.out.println("Done");
 	}
 
+	private void findInstructor(AppDAO appDAO) {
+		int theId = 1;
+		System.out.println("Finding instructor id: " + theId);
+
+		Instructor tempInstructor = appDAO.findInstructorById(theId);
+
+		System.out.println("tempInstructor: " + tempInstructor);
+		System.out.println("the associate instructorDetail only: " + tempInstructor.getInstructorDetail());
+	}
 }
--- src/main/java/com/maxiflexy/advancedhibernate/dao/AppDAOImpl.java
+++ src/main/java/com/maxiflexy/advancedhibernate/dao/AppDAOImpl.java
@@ -19,4 +19,9 @@ public class AppDAOImpl implements AppDAO{
         entityManager.persist(theInstructor);
 
     }
+
+    @Override
+    public Instructor findInstructorById(int theId) {
+        return entityManager.find(Instructor.class, theId);
+    }
 }
